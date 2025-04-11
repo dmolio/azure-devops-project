0c54f62bb38c2375180d9c1cd26fb7235067489b
dmolio <ewambedarryl@yahoo.ca>
2025-04-10 22:38:14 -0400

    Added AKS and ACR configurations for sample app deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,24 @@ resource "azurerm_subnet" "subnet" {
 
 # Placeholder for AKS cluster configuration
 # This will be expanded based on specific requirements
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "azure-devops-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "azuredevopsaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "resource_group_id" {
 output "vnet_id" {
   value = azurerm_virtual_network.vnet.id
 }
+
+output "aks_kubeconfig" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
