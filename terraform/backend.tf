b8c0ee7b6ec0b29cb4b92b690cb531f8ed23184b
dmolio <ewambedarryl@yahoo.ca>
2025-04-10 23:13:18 -0400

    Updated Terraform configuration for Service Principal authentication

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,3 +6,35 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+
+provider "azurerm" {
+  features {}
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
+
+variable "azure_client_id" {
+  type        = string
+  description = "Azure Service Principal Client ID"
+  sensitive   = true
+}
+
+variable "azure_client_secret" {
+  type        = string
+  description = "Azure Service Principal Client Secret"
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+  sensitive   = true
+}
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+  sensitive   = true
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+# Terraform variables file for Azure credentials
+# These values will be set by GitHub Secrets in the CI/CD pipeline
+azure_client_id       = ""
+azure_client_secret   = ""
+azure_tenant_id       = ""
+azure_subscription_id = ""
