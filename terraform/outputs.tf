b4841fcdcf64206a49810d3b61eead427419a8f9
dmolio <ewambedarryl@yahoo.ca>
2025-04-10 22:42:00 -0400

    Fixed formatting in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,6 +7,6 @@ output "vnet_id" {
 }
 
 output "aks_kubeconfig" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
